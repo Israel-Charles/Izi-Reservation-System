5598ddeadaa17f071bffe619e8a1c3da22aec848
keating <keatingsane@outlook.com>
2024-07-22 13:30:25 -0400

    res. form start

--- frontend/src/forms/ReservationForm/ReservationForm.tsx
+++ frontend/src/forms/ReservationForm/ReservationForm.tsx
@@ -1,4 +1,7 @@
+import { zoomies } from "ldrs";
 import { useForm } from "react-hook-form";
+import { BackButton } from "../../components/Buttons";
+import { useEffect } from "react";
 
 type Props = {
     resourceId: string;
@@ -18,6 +21,68 @@ const ReservationForm = ({ resourceId }: Props) => {
         handleSubmit,
         formState: { errors },
     } = useForm<ReservationFormData>();
+
+    useEffect(() => {
+        zoomies.register();
+    }, []);
+
+    return (
+        <div className="flex flex-col gap-6">
+            <form
+                onSubmit={handleSubmit((data) => {
+                    console.log(data);
+                })}
+                className="flex flex-col gap-4"
+            >
+                <span className="text-lg font-semibold">Reservation form</span>
+                <textarea
+                    placeholder="Comment"
+                    className="rounded bg-background p-2"
+                    {...register("comment")}
+                />
+                <input
+                    type="time"
+                    placeholder="Start"
+                    className="rounded bg-background p-2"
+                    {...register("start")}
+                />
+                <input
+                    type="time"
+                    placeholder="End"
+                    className="rounded bg-background p-2"
+                    {...register("end")}
+                />
+                <input
+                    type="number"
+                    placeholder="Size"
+                    className="rounded bg-background p-2"
+                    {...register("size")}
+                />
+            </form>
+            <span className="flex justify-between">
+                <BackButton />
+                <button
+                    disabled
+                    type="submit"
+                    className="rounded text-xl text-light_neutral bg-med_orange font-bold px-3 py-2 hover:bg-background hover:text-med_orange hover:shadow-lg disabled:bg-tertiary disabled:text-secondary transition-all"
+                >
+                    {/* { ? ( */}
+                    <div className="py-1/2">
+                        <l-zoomies
+                            size="50"
+                            stroke="15"
+                            bg-opacity="0.1"
+                            speed="1.4"
+                            color="rgb(255, 125, 40)"
+                        ></l-zoomies>
+                    </div>
+                    {/* ) : (
+                        <span>Save</span>
+                    )} */}
+                </button>
+            </span>
+        </div>
+    );
 };
 
 export default ReservationForm;
--- frontend/src/pages/Reserve.tsx
+++ frontend/src/pages/Reserve.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
-import { BackButton } from "../components/Buttons";
+import ReservationForm from "../forms/ReservationForm/ReservationForm";
 
 const Reserve = () => {
     const { resourceId } = useParams();
@@ -10,46 +10,43 @@ const Reserve = () => {
         () => apiClient.getResourceById(resourceId as string),
         { enabled: !!resourceId }
     );
-
     if (!resource) {
         return <></>;
     }
+
     return (
         <div className="container mx-auto p-6 my-14">
-            <div className="bg-background_alt rounded-lg shadow-lg p-6 text-primary">
-                <div className="flex justify-start items-center gap-3">
-                    <span className="text-xl md:text-2xl lg:text-3xl font-bold">
-                        {resource.name}
-                    </span>
-                    <span className="text-sm md:text-base lg:text-lg text-secondary">
-                        {resource.type}
-                    </span>
-                </div>
-                <span className="text-base md:text-lg lg:text-xl text-secondary">
-                    {resource.location}
-                </span>
-                <span className="flex justify-between">
-                    <BackButton />
-                    <button
-                        disabled
-                        type="submit"
-                        className="rounded text-xl text-light_neutral bg-med_orange font-bold px-3 py-2 hover:bg-background hover:text-med_orange hover:shadow-lg disabled:bg-tertiary disabled:text-secondary transition-all"
-                    >
-                        {/* { ? ( */}
-                        <div className="py-1/2">
-                            <l-zoomies
-                                size="50"
-                                stroke="15"
-                                bg-opacity="0.1"
-                                speed="1.4"
-                                color="rgb(255, 125, 40)"
-                            ></l-zoomies>
+            <div className="flex flex-col gap-y-6 bg-background_alt rounded-lg shadow-lg p-6 text-primary">
+                <div>
+                    <div className="flex justify-start items-center gap-3">
+                        <span className="text-xl md:text-2xl lg:text-3xl font-bold">
+                            {resource.name}
+                        </span>
+                        <span className="text-sm md:text-base lg:text-lg text-secondary">
+                            {resource.type}
+                        </span>
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        <span className="text-base md:text-lg lg:text-xl text-secondary">
+                            {resource.location}
+                        </span>
+                        <div className="flex justify-start items-center gap-3">
+                            <span className="text-sm md:text-base text-secondary">
+                                {resource.days.join(", ")}
+                            </span>
+                            <span className="text-sm md:text-base text-secondary">
+                                {resource.open} - {resource.close}
+                            </span>
                         </div>
-                        {/* ) : (
-                        <span>Save</span>
-                    )} */}
-                    </button>
+                        <span className="text-sm md:text-base text-secondary">
+                            {resource.description}
+                        </span>
+                    </div>
+                </div>
+                <span className="font-extrabold text-blue-500">
+                    calendar views will go here
                 </span>
+                <ReservationForm resourceId={resourceId} />
             </div>
         </div>
     );
